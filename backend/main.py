0a7b3f1704e95c0ecddd310d980996a18ff00f1e
RumbleJack56 <ujjwalkakar2005@gmail.com>
2024-10-24 00:04:34 +0530

    logs table routes added

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 from routers import users
-import logging
 from routers import stocks
 
-
-logging.basicConfig(level=logging.DEBUG)
-app = FastAPI(debug=True)
+app = FastAPI()
 
 app.include_router(users.router, prefix="/user")
 app.include_router(stocks.router, prefix= "/stocks")
--- backend/routers/stocks.py
+++ backend/routers/stocks.py
@@ -18,7 +18,13 @@ async def add_stock(uuid: str, stock_name: str, qty: int, stock_price: float):
         'last_transaction_date' : datetime.now().strftime(r"%Y-%m-%d") 
         
     }).execute()
-
+    supabase.table('logs').insert({
+        'stock_id' : response.data[0]['stock_id'],
+        'qty' :qty,
+        'total_qty' : qty,
+        'current_price' : stock_price,
+        'final_investment' : stock_price * qty
+    }).execute()
     return {"message": "Stock added successfully", "data": response.data}
 
 #fetchinggg
@@ -31,7 +37,7 @@ async def user_stocks(uuid: str):
 # 2. Updating stocks
 @router.get("/update_stock/")
 async def update_stock(stock_id: str, qty: int, price: float):
-    existing_stock = supabase.table('stocks').select("*").eq('stock_id', stock_id).execute().data
+    existing_stock = supabase.table('stocks').select("*").eq('stock_id', stock_id).execute().data[0]
     
     update_data = {
         'qty': qty + existing_stock['qty'],
@@ -40,5 +46,27 @@ async def update_stock(stock_id: str, qty: int, price: float):
     }
 
     response = supabase.table('stocks').update(update_data).eq('stock_id', stock_id).execute()
-
+    supabase.table('logs').insert({
+        'stock_id' : response.data[0]['stock_id'],
+        'qty' :qty,
+        'total_qty' : response.data[0]['qty'],
+        'current_price' : price,
+        'final_investment' : response.data[0]['investment']
+    }).execute()
     return {"message": "Stock updated successfully", "data": response.data}
+
+@router.get("/sell_stock")
+async def sell_stock(stock_id: str , price: float):
+    existing_stock = supabase.table('stocks').select("*").eq('stock_id', stock_id).execute().data[0]
+    supabase.table('logs').delete().eq('stock_id', stock_id).execute()
+    supabase.table('logs').insert({
+        'stock_id' : existing_stock['stock_id'],
+        'qty' : existing_stock['qty'],
+        'total_qty' : 0,
+        'current_price' : price,
+        'final_investment' : existing_stock['investment'] - existing_stock['qty'] * price,
+    }).execute()
+    return {"message": "Stock sold successfully", "final_sell_price": existing_stock['qty'] * price , "data": existing_stock}
+
+    
+    
